8aaf133d5fd20138db0c155386d4a312406c6cb8
Arman Hossain <armanhossaincse7@gmail.com>
2022-11-23 21:17:33 +0600

    full 34

---
+++ wp-content/themes/aquila/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Footer file.
+ * 
+ * @package Aquila
+ * 
+ */
+?>
+   <footer>
+      <h2>Footer</h2>
+   </footer>
+   </div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/aquila/single.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Single page template file.
+ * 
+ * @package Aquila
+ */
+get_header();
+?>
+
+ <div class="content">
+   <?php esc_html_e( 'Signle Page', 'aquila' ); ?>
+ </div>
+   
+<?php 
+get_footer();

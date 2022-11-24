edd54326a60ccd6cfd4d1e4309224c38c37fba35
Arman Hossain <armanhossaincse7@gmail.com>
2022-11-24 23:42:40 +0600

    Customize Excerpt Length WordPress | wp_trim_excerpt | excerpt

--- wp-content/themes/aquila/template-parts/components/blog/entry-content.php
+++ wp-content/themes/aquila/template-parts/components/blog/entry-content.php
@@ -1,8 +1,33 @@
 <?php
 /**
- * Template for post entry header
+ * Template for post entry content
+ * To be used inside wordpress the loop
  * 
  * @package aquila
  */
 
 ?>
+
+<div class="entry-content">
+   <?php
+      if ( is_single() ){
+         the_content(
+            sprintf(
+               wp_kses(
+                  __( 'Continue reading %s <span class="meta-nav">&rarrr;</span>', 'aquila' ),
+                  [
+                     'span' => [
+                        'class' => []
+                     ]
+                  ]
+
+               ),
+               the_title( '<span class=" >"', '</span>', false )
+            )
+         );
+      }else{
+         aquila_the_excerpt( 200 );
+      }
+   ?>
+</div>
+

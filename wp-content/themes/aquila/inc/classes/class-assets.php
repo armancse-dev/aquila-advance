4deeee61ea1829acc54a9f6c8d1c9d143facf968
Arman Hossain <armanhossaincse7@gmail.com>
2022-12-03 20:02:57 +0600

    @babel/preset-env

--- wp-content/themes/aquila/inc/classes/class-assets.php
+++ wp-content/themes/aquila/inc/classes/class-assets.php
@@ -4,8 +4,8 @@
  * @package Aquila
  */
 
- namespace Aquila_Theme\Inc;
- use AQUILA_THEME\Inc\Traits\Singleton;
+namespace AQUILA_THEME\Inc;
+use AQUILA_THEME\Inc\Traits\Singleton;
 
  class Assets {
     use Singleton;
@@ -27,20 +27,20 @@
     }
 
     public function register_styles(){
-        //register styles
-        wp_register_style( 'style', get_stylesheet_uri(), [], filemtime( AQUILA_DIR_PATH. '/style.css' ), 'all' );
+       //register styles
+       wp_register_style( 'style', get_stylesheet_uri(), [], filemtime( get_template_directory_uri() . '/style.css' ), 'all' );
 
-        // wp_register_style( 'main-css', AQUILA_BUILD_CSS_URI . '/style.css', ['main-css'], filemtime( AQUILA_BUILD_CSS_DIR_PATH . '/style.css' ), 'all' );
+       // wp_register_style( 'main-css', AQUILA_BUILD_CSS_URI . '/style.css', ['main-css'], filemtime( AQUILA_BUILD_CSS_DIR_PATH . '/style.css' ), 'all' );
 
-        wp_register_style( 'bootstrap-css', AQUILA_DIR_URI. '/assets/src/library/css/bootstrap.min.css' , [], false, 'all' );
-         //enqueue style
-        wp_enqueue_style('main-css');
-        wp_enqueue_style('bootstrap-css');
+       wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css' , [], false, 'all' );
+        //enqueue style
+       wp_enqueue_style('main-css');
+       wp_enqueue_style('bootstrap-css');
     }
 
     public function register_scripts(){
         //register scripts
-        wp_register_script( 'main-script', AQUILA_BUILD_JS_URI . '/main.js', [], filemtime( AQUILA_BUILD_JS_URI. '/assets/main.js' ), true );
+        wp_register_script( 'main-script', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory(). '/assets/main.js' ), true );
         
         wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], true );
 
